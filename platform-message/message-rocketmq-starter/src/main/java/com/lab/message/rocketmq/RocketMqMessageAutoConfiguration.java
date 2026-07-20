197ba70cce7c155b2680eccfe03f25ec834f52b1
高明山 <yamasama66@gmail.com>
2026-07-20 18:34:19 +0800

    重构消息

--- platform-message/message-rocketmq-starter/src/main/java/com/lab/message/rocketmq/RocketMqMessageAutoConfiguration.java
+++ platform-message/message-rocketmq-starter/src/main/java/com/lab/message/rocketmq/RocketMqMessageAutoConfiguration.java
@@ -1,30 +1,19 @@
 package com.lab.message.rocketmq;
 
-import com.lab.message.contract.DelayedEventPublisher;
-import com.lab.message.contract.EventPublisher;
-import com.lab.message.contract.EventSubscriber;
-import com.lab.message.contract.OrderedEventPublisher;
-import com.lab.message.contract.TransactionalEventPublisher;
-import com.lab.message.rocketmq.adapter.RocketMqDelayedProducer;
 import com.lab.message.rocketmq.adapter.RocketMqMessageMapper;
 import com.lab.message.rocketmq.adapter.RocketMqDestinationResolver;
-import com.lab.message.rocketmq.adapter.RocketMqOrderedProducer;
-import com.lab.message.rocketmq.adapter.RocketMqEventPublisher;
-import com.lab.message.rocketmq.adapter.RocketMqTransport;
-import com.lab.message.rocketmq.adapter.RocketMqTransactionalProducer;
-import com.lab.message.rocketmq.adapter.RocketMqEventSubscriber;
+import com.lab.message.rocketmq.adapter.RocketMqMessageFacade;
 import org.apache.rocketmq.spring.autoconfigure.RocketMQProperties;
 import org.apache.rocketmq.spring.support.RocketMQMessageConverter;
-import org.apache.rocketmq.spring.core.RocketMQLocalTransactionListener;
+import org.apache.rocketmq.spring.support.RocketMQMessageListenerContainerRegistrar;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @AutoConfiguration
 @EnableConfigurationProperties(RocketMqMessageProperties.class)
@@ -45,45 +34,13 @@ public class RocketMqMessageAutoConfiguration {
     }
 
     @Bean
-    @ConditionalOnBean(RocketMQTemplate.class)
-    @ConditionalOnMissingBean(RocketMqTransport.class)
-    RocketMqTransport rocketMqTransport(RocketMQTemplate template, RocketMqMessageMapper mapper,
-                                        RocketMqMessageProperties properties) {
-        return new RocketMqTransport(template, mapper, properties.getDelayLevels());
-    }
-
-    @Bean(destroyMethod = "close")
-    @ConditionalOnBean({RocketMQProperties.class, RocketMQMessageConverter.class})
-    @ConditionalOnMissingBean(EventSubscriber.class)
-    EventSubscriber rocketMqEventSubscriber(RocketMQProperties properties,
-                                            RocketMQMessageConverter messageConverter,
-                                            ApplicationContext applicationContext) {
-        return new RocketMqEventSubscriber(properties, messageConverter, applicationContext);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(EventPublisher.class)
-    EventPublisher rocketMqEventPublisher(RocketMqTransport transport) {
-        return new RocketMqEventPublisher(transport);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(OrderedEventPublisher.class)
-    OrderedEventPublisher rocketMqOrderedEventPublisher(RocketMqTransport transport) {
-        return new RocketMqOrderedProducer(transport);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(DelayedEventPublisher.class)
-    @ConditionalOnDelayLevels
-    DelayedEventPublisher rocketMqDelayedEventPublisher(RocketMqTransport transport) {
-        return new RocketMqDelayedProducer(transport);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(TransactionalEventPublisher.class)
-    @ConditionalOnBean({RocketMQTemplate.class, RocketMQLocalTransactionListener.class})
-    TransactionalEventPublisher rocketMqTransactionalEventPublisher(RocketMqTransport transport) {
-        return new RocketMqTransactionalProducer(transport);
+    @ConditionalOnMissingBean(RocketMqMessageFacade.class)
+    RocketMqMessageFacade rocketMqMessageFacade(
+            RocketMQTemplate template,
+            RocketMqMessageMapper mapper,
+            RocketMqMessageProperties properties,
+            RocketMQMessageListenerContainerRegistrar registrar,
+            RocketMQProperties rocketMqProperties) {
+        return new RocketMqMessageFacade(template, mapper, properties.getDelayLevels(), registrar, rocketMqProperties);
     }
 }
