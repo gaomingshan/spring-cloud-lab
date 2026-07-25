20a453e81286c420568175e36debaa35d73be392
高明山 <yamasama66@gmail.com>
2026-07-25 22:55:25 +0800

    消息重构

--- platform-message/message-rocketmq-adapter/src/main/java/com/lab/message/rocketmq/adapter/RocketMqMessageMapper.java
+++ platform-message/message-rocketmq-adapter/src/main/java/com/lab/message/rocketmq/adapter/RocketMqMessageMapper.java
@@ -7,23 +7,20 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 
 public final class RocketMqMessageMapper {
+
     public Message<?> map(BaseEvent event) {
         if (event == null) {
             throw new MessageException("VALIDATION_FAILED: event is null");
         }
-        if (event.getClass() == BaseEvent.class
-                || java.lang.reflect.Modifier.isAbstract(event.getClass().getModifiers())) {
-            throw new MessageException("VALIDATION_FAILED: publish requires a concrete event type");
-        }
         return MessageBuilder.withPayload(event)
-                .setHeader(RocketMQHeaders.KEYS, event.resolveIdempotencyKey())
-                .setHeader("lab.event-id", event.getEventId())
-                .setHeader("lab.event-class", event.getClass().getName())
-                .setHeader("lab.event-type", event.getEventType())
-                .setHeader("lab.producer", event.getProducer())
-                .setHeader("lab.aggregate-type", event.getAggregateType())
-                .setHeader("lab.aggregate-id", event.getAggregateId())
-                .copyHeaders(event.getHeaders())
-                .build();
+            .setHeader(RocketMQHeaders.KEYS, event.getEventId())
+            .setHeader("lab.event-id", event.getEventId())
+            .setHeader("lab.event-class", event.getClass().getName())
+            .setHeader("lab.event-type", event.getEventType())
+            .setHeader("lab.producer", event.getProducer())
+            .setHeader("lab.aggregate-type", event.getAggregateType())
+            .setHeader("lab.aggregate-id", event.getAggregateId())
+            .copyHeaders(event.getHeaders())
+            .build();
     }
 }
